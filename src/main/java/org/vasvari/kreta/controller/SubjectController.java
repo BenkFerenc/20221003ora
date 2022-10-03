838e3467fae16848fd1b62265d0d4032a79b89ae
sze1benfer@vasvari.org <sze1benfer@vasvari.org>
2022-10-03 20:32:15 +0200

    jegyzetírás

--- src/main/java/org/vasvari/kreta/controller/SubjectController.java
+++ src/main/java/org/vasvari/kreta/controller/SubjectController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @Controller
 public class SubjectController {
+    //ezt csináltuk 1003 órán elősször
     @Autowired
     SubjectService service;
     @GetMapping("/subjects/index")
